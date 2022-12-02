28a2f79e8badfb92fca0983c089b6021aa9c7d29
Adam <adamtemplin92@gmail.com>
2022-12-02 14:44:24 +0100

    Day18 - Add exit button and error popup

--- 16_bonus/e1.py
+++ 16_bonus/e1.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 
+sg.theme('DarkBlack1')
+
 label1 = sg.Text('Enter feet:')
 input1 = sg.InputText(key='feet')
 error_label1 = sg.Text('', key='feet_error')
@@ -10,9 +12,10 @@ input2 = sg.InputText(key='inches')
 error_label2 = sg.Text('', key='inches_error')
 layout.append([label2, input2, error_label2])
 
-compress_button = sg.Button("Convert")
+convert_button = sg.Button('Convert')
+exit_button = sg.Button('Exit')
 output_label = sg.Text('', key='output')
-layout.append([compress_button, output_label])
+layout.append([convert_button, exit_button, output_label])
 
 window = sg.Window('Converter', layout=layout)
 
@@ -38,10 +41,12 @@ while True:
             window['output'].update(value='One or both above numbers are incorrect')
 
         try:
-            result = (feet*12 + inches)*2.54/100
+            result = f'{(feet*12 + inches)*2.54/100} m'
         except:
             result = 'One or both above numbers are incorrect'
-
+            sg.popup('One or both above numbers are incorrect')
         window['output'].update(value=result)
+    elif event == 'Exit':
+        break
 
 window.close()
